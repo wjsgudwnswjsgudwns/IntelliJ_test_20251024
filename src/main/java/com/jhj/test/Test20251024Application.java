926a120562c01c00ac8a4b3523ae828c343628d6
wjsgudwnswjsgudwns <wns10099@naver.com>
2025-10-24 11:39:17 +0900

    기본 프로젝트 생성

---
+++ src/main/java/com/jhj/test/Test20251024Application.java
@@ -0,0 +1,13 @@
+package com.jhj.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test20251024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test20251024Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jhj/test/Test20251024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhj.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test20251024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
